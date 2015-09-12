e832353a55edc3a453a8795f5b1b9226bb56a5e6
Devine Lu Linvega <aliceffekt@gmail.com>
2015-09-12 12:50:36 -0400

    Mockup

--- monitor/monitor/AppDelegate.swift
+++ monitor/monitor/AppDelegate.swift
@@ -14,20 +14,48 @@ class AppDelegate: NSObject, NSApplicationDelegate
 	@IBOutlet var statusMenu: NSMenu?
 	var statusBarItem : NSStatusItem?
 	
+	@IBOutlet weak var menu: NSMenu!
 	@IBOutlet weak var window: NSWindow!
 	
 	override func awakeFromNib() {
 		let statusBar = NSStatusBar.systemStatusBar()
-		statusBarItem = statusBar.statusItemWithLength(CGFloat(NSVariableStatusItemLength))
+		statusBarItem = statusBar.statusItemWithLength(50)
 		statusBarItem!.menu = statusMenu
-		statusBarItem!.title = "TestApp"
+		statusBarItem!.title = "2.9k"
+		
+		let image = NSImage(named: "icon.png")
+		image?.size = NSSize(width: 18, height: 18)
+		statusBarItem?.image = image
+
+	}
+	
+	@IBAction func optionOscean(sender: AnyObject)
+	{
+		
+	}
+	
+	@IBAction func optionQuit(sender: AnyObject)
+	{
+		
+	}
+	
+	@IBAction func optionParadise(sender: AnyObject)
+	{
+		
+	}
+	
+	@IBAction func optionOsceanLast(sender: AnyObject)
+	{
+		
 	}
 
-	func applicationDidFinishLaunching(aNotification: NSNotification) {
+	func applicationDidFinishLaunching(aNotification: NSNotification)
+	{
 		// Insert code here to initialize your application
 	}
 
-	func applicationWillTerminate(aNotification: NSNotification) {
+	func applicationWillTerminate(aNotification: NSNotification)
+	{
 		// Insert code here to tear down your application
 	}
 }
