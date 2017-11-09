10260ecd3536a5f875ea7c3120cd7c20e4f40ef2
Devine Lu Linvega <aliceffekt@gmail.com>
2017-11-09 21:42:10 +1300

    *

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const {app, Menu, Tray} = require('electron')
 const nativeImage = require('electron').nativeImage
-let image = nativeImage.createEmpty()
+let image = nativeImage.createFromPath("glyphs/t.891.png") // nativeImage.createEmpty()
 
 function Clock()
 {
@@ -40,7 +40,7 @@ function Clock()
     return this.show_pulse ? t_s.substr(0,3)+":"+t_s.substr(3,3) : t_s.substr(0,3);
   }
 
-  this.draw = function()
+  this.path = function()
   {
     var t        = this.time();
     var t_s      = new String(t);
@@ -49,10 +49,10 @@ function Clock()
     var h        = 30;
     var needle_1 = parseInt(((t/1000000) % 1) * w);
     var needle_2 = parseInt(((t/100000) % 1) * h);
-    var needle_3 = needle_1 + parseInt(((t/10000)) * (w - needle_1));
-    var needle_4 = needle_2 + parseInt(((t/10000)) * (h - needle_2));
-    var needle_5 = needle_3 + parseInt(((t/1000)) * (w - needle_3));
-    var needle_6 = needle_4 + parseInt(((t/100)) * (h - needle_4));
+    var needle_3 = needle_1 + parseInt(((t/10000) % 1) * (w - needle_1));
+    var needle_4 = needle_2 + parseInt(((t/10000) % 1) * (h - needle_2));
+    var needle_5 = needle_3 + parseInt(((t/1000) % 1) * (w - needle_3));
+    var needle_6 = needle_4 + parseInt(((t/100) % 1) * (h - needle_4));
 
     var path = "";
     path += "M"+needle_1+","+0+" L"+needle_1+","+h+" ";
@@ -68,7 +68,7 @@ function Clock()
     path += "M"+(w-0.5)+","+(h-0.5)+" L0.5,"+(h-0.5)+" ";
     path += "M"+(w-1)+","+(h-0.5)+" L"+(w-1)+",0.5 ";
 
-    return '<svg width="'+w+'" height="'+h+'" style="fill:white; stroke-width:1; stroke-linecap:butt; stroke:white; background:white"><path class="fh" d="'+path+'"></path></svg>';
+    return path;
   }
 }
 
@@ -80,18 +80,21 @@ app.on('ready', () => {
 
   clock.start()
 
-  const contextMenu = Menu.buildFromTemplate([
-    {label: 'Toggle Pulse', click: () => clock.toggle_pulse() },
-    {label: 'Quit'}
-  ])
-
-  tray.setToolTip('This is my application.')
   tray.setTitle("---");
 
-  var str = '<svg width="20" height="20" xmlns="http://www.w3.org/2000/svg" baseProfile="full" version="1.1" style="fill:white;stroke:black;stroke-width:0px;stroke-linecap:round;"><path d="M0,0 L20,0 L20,20 L0,20 Z "></path></svg>';
-
-  function update(){
-    tray.setTitle(clock.format());
+  var prev_beat = null;
+  function update(force = false){
+    var time = clock.format();
+    var beat = time.substr(0,3);
+    if(clock.show_pulse || force){
+      tray.setTitle(" "+time);  
+    }
+    else if(prev_beat != beat){
+      let image = nativeImage.createFromPath("glyphs/t."+beat+".png")
+      tray.setImage(image);
+      tray.setTitle(" "+time);
+    }
+    prev_beat = beat;
   }
 
   setInterval(update,86.40)
@@ -99,9 +102,11 @@ app.on('ready', () => {
   tray.setHighlightMode(false)
 
   tray.on('click', function(e, bounds) {
-    console.log('clicked'); // doesn't fire after quick successive clicks
+    console.log(clock.path())
+    clock.toggle_pulse();
+    update(true)
   });
-  tray.setContextMenu(contextMenu)
+
   update();
 })
 
