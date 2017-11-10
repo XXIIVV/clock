e128224c8f39184881034fd48fdad845bc89baaa
Devine Lu Linvega <aliceffekt@gmail.com>
2017-11-10 16:45:19 +1300

    Improved dropdown

--- main.js
+++ main.js
@@ -1,18 +1,12 @@
 const {app, Menu, Tray} = require('electron')
 const nativeImage = require('electron').nativeImage
-let image = nativeImage.createFromPath("glyphs/t.891.png") // nativeImage.createEmpty()
+let image = nativeImage.createEmpty()
 
 function Clock()
 {
   this.radius = 90;
   this.circ = this.radius * 2 * Math.PI;
   this.center = 105;
-  this.show_pulse = false;
-
-  this.start = function()
-  {
-    clock.update();
-  }
 
   this.time = function()
   {
@@ -22,53 +16,11 @@ function Clock()
     return parseInt(val);
   }
 
-  this.toggle_pulse = function()
-  {
-    this.show_pulse = this.show_pulse ? false : true;
-  }
-
-  this.update = function()
-  {
-    setTimeout(function(){ clock.update(); }, 864.0);
-  }
-
   this.format = function()
   {
     var t        = this.time();
     var t_s      = new String(t);
-
-    return this.show_pulse ? t_s.substr(0,3)+":"+t_s.substr(3,3) : t_s.substr(0,3);
-  }
-
-  this.path = function()
-  {
-    var t        = this.time();
-    var t_s      = new String(t);
-    var t_a      = [t_s.substr(0,3),t_s.substr(3,3)];
-    var w        = 30;
-    var h        = 30;
-    var needle_1 = parseInt(((t/1000000) % 1) * w);
-    var needle_2 = parseInt(((t/100000) % 1) * h);
-    var needle_3 = needle_1 + parseInt(((t/10000) % 1) * (w - needle_1));
-    var needle_4 = needle_2 + parseInt(((t/10000) % 1) * (h - needle_2));
-    var needle_5 = needle_3 + parseInt(((t/1000) % 1) * (w - needle_3));
-    var needle_6 = needle_4 + parseInt(((t/100) % 1) * (h - needle_4));
-
-    var path = "";
-    path += "M"+needle_1+","+0+" L"+needle_1+","+h+" ";
-    path += "M"+needle_1+","+needle_2+" L"+w+","+needle_2+" ";
-    path += "M"+needle_3+","+needle_2+" L"+needle_3+","+h+" ";
-    path += "M"+needle_3+","+needle_4+" L"+w+","+needle_4+" ";
-    path += "M"+needle_5+","+needle_4+" L"+needle_5+","+h+" ";
-    path += "M"+needle_5+","+needle_6+" L"+w+","+needle_6+" ";
-
-    // Outline
-    path += "M0.5,0.5 L"+(w-0.5)+",0.5 ";
-    path += "M0.5,0.5 L0.5,"+(h-0.5)+" ";
-    path += "M"+(w-0.5)+","+(h-0.5)+" L0.5,"+(h-0.5)+" ";
-    path += "M"+(w-1)+","+(h-0.5)+" L"+(w-1)+",0.5 ";
-
-    return path;
+    return t_s.substr(0,3)+":"+t_s.substr(3,3);
   }
 }
 
@@ -76,40 +28,31 @@ let clock = new Clock()
 
 app.on('ready', () => {
 
-  var tray = new Tray(image)
-
-  clock.start()
-
-  tray.setTitle("---");
-
-  var prev_beat = null;
-  function update(force = false){
+  function update(pulse)
+  {
     var time = clock.format();
     var beat = time.substr(0,3);
-    if(clock.show_pulse || force){
-      tray.setTitle(" "+time);  
+    if(menu && menu.items[1].checked){
+      tray.setTitle(time);
     }
-    else if(prev_beat != beat){
-      let image = nativeImage.createFromPath("glyphs/t."+beat+".png")
-      tray.setImage(image);
-      tray.setTitle(" "+time);
+    else if(prev_beat != beat || pulse == 1){
+      tray.setTitle(beat);
     }
     prev_beat = beat;
   }
 
+  var prev_beat = null;
+  var tray = new Tray(image)
+  var menu = Menu.buildFromTemplate([
+    {label: '000', type: 'radio', checked: true, click:function(){ update(1) } },
+    {label: '000:000', type: 'radio', click:function(){ update(2) } },
+    {label: 'Quit', click:function(){ app.quit(); } }
+  ])
+  tray.setContextMenu(menu)
+
+  tray.setTitle("---");
+
   setInterval(update,86.40)
 
-  tray.setHighlightMode(false)
-
-  tray.on('click', function(e, bounds) {
-    console.log(clock.path())
-    clock.toggle_pulse();
-    update(true)
-  });
-
-  update();
+  update(1);
 })
-
-
-
-
