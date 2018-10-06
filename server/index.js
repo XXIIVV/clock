2436b1832dc31221b97282000075b009678e475a
Devine Lu Linvega <aliceffekt@gmail.com>
2018-10-07 09:36:50 +1200

    Optimized

--- server/index.js
+++ server/index.js
@@ -1,20 +1,21 @@
 'use strict'
 
-function desamber()
+function desamber(date = new Date)
 {
-  const date = new Date
-  const start = new Date(date.getFullYear(), 0, 0);
-  const diff = (date - start) + ((start.getTimezoneOffset() - date.getTimezoneOffset()) * 60 * 1000);
-  const doty = Math.floor(diff/86400000);
-  let y = date.getFullYear().toString().substr(2,2);
-  let m = String.fromCharCode(97 + Math.floor(((doty-1)/364) * 26)).toUpperCase(); m = doty == 365 || doty == 366 ? "+" : m;
-  let d = (doty % 14); d = d < 10 ? `0${d}` : d; d = d == "00" ? "14" : d; d = doty == 365 ? "01" : (doty == 366 ? "02" : d);
+  const start = new Date(date.getFullYear(), 0, 0)
+  const diff = (date - start) + ((start.getTimezoneOffset() - date.getTimezoneOffset()) * 60 * 1000)
+  const doty = Math.floor(diff/86400000)-1
+  const l = Math.floor(((doty)/364) * 26)
+  const y = date.getFullYear().toString().substr(2,2)
+  const m = doty == 365 || doty == 366 ? "+" : String.fromCharCode(97 + l).toUpperCase()
+  const d = `${(doty == 365 ? 1 : doty == 366 ? 2 : (doty % 14))+1}`.padStart(2,"0")
   return `${y}${m}${d}`;
 }
 
 function neralie()
 {
-  const d = new Date(); const e = new Date(d)
+  const d = new Date()
+  const e = new Date(d)
   const t = (e - d.setHours(0, 0, 0, 0) / 8640) * 100
   return `${t}`.substr(0, 3) + ':' + `${t}`.substr(3, 3)
 }
