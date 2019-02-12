43b2cd22e4290fea78849f4a7abdebe7d2faf33d
Devine Lu Linvega <aliceffekt@gmail.com>
2019-02-12 12:24:38 +1200

    Removed non-web tools

--- web/scripts/clock.js
+++ web/scripts/clock.js
@@ -1,33 +1,27 @@
-function Clock()
-{
-  this.show_pulse = false;
+function Clock () {
+  this.show_pulse = false
 
-  this.toggle_pulse = function()
-  {
-    this.show_pulse = this.show_pulse ? false : true;
+  this.toggle_pulse = function () {
+    this.show_pulse = !this.show_pulse
   }
 
-  this.time = function()
-  {
-    var d = new Date(), e = new Date(d);
-    var msSinceMidnight = e - d.setHours(0,0,0,0);
-    return (msSinceMidnight / 8640) * 100;
+  this.time = function () {
+    var d = new Date(); var e = new Date(d)
+    var msSinceMidnight = e - d.setHours(0, 0, 0, 0)
+    return (msSinceMidnight / 8640) * 100
   }
 
-  this.format = function()
-  {
-    var t        = this.time();
-    var t_s      = new String(t);
-    return {beat:t_s.substr(0,3),pulse:t_s.substr(3,3)};
+  this.format = function () {
+    var t = this.time()
+    var t_s = new String(t)
+    return { beat: t_s.substr(0, 3), pulse: t_s.substr(3, 3) }
   }
 
-  this.toString = function()
-  {
+  this.toString = function () {
     return `${this.format().beat}:${this.format().pulse}`
   }
 
-  this.menu = function(app)
-  {
-    return {label: 'Show Pulse', type: 'checkbox', checked: this.show_pulse, click:() => { this.toggle_pulse(); app.update_menu(); } };
+  this.menu = function (app) {
+    return { label: 'Show Pulse', type: 'checkbox', checked: this.show_pulse, click: () => { this.toggle_pulse(); app.update_menu() } }
   }
 }
---
+++ web/scripts/neralie.js
@@ -0,0 +1,23 @@
+'use strict'
+
+function neralie (date = new Date()) {
+  const e = new Date(date)
+  const ms = e - date.setHours(0, 0, 0, 0)
+  const val = (ms / 8640 / 10000).toFixed(6)
+  return `${val.substr(2, 3)}:${val.substr(5, 3)}`
+}
+
+function Neralie (t = neralie()) {
+  this.beat = t.split(':')[0]
+  this.pulse = t.split(':')[1]
+
+  this.toInteger = function (date = new Date()) {
+    const e = new Date(date)
+    const ms = e - date.setHours(0, 0, 0, 0)
+    return (ms / 8640 / 10000).toFixed(6)
+  }
+
+  this.toString = function () {
+    return t
+  }
+}
