a68d611110dcd1df76fd770d4621f4645b0bf58b
neauoire <aliceffekt@gmail.com>
2020-04-17 20:44:00 +0900

    Added pascal

--- pascal/neralie.pas
+++ pascal/neralie.pas
@@ -4,12 +4,15 @@ program Neralie;
   pad = 20;
   x = 60;
   y = 60;
-  width = 600;
-  height = 400;
+  width = 300;
+  height = 200;
 
  var
   view, display, bounds: Rect;
+  dtr: DateTimeRec;
+  res, last: longInt;
 
+{>> Draw }
  procedure draw (bounds: rect; a, b, c, d, e, f: real);
   var
    x, y, prev: integer;
@@ -36,19 +39,45 @@ program Neralie;
   drawLine(x, y, x, bounds.bottom - 1);
  end;
 
+{>> toNeralie }
+ function toNeralie (dtr: DateTimeRec): LongInt;
+  var
+   seconds: LongInt;
+   ratio: extended;
+ begin
+  seconds := 3600;
+  seconds := seconds * dtr.hour;
+  seconds := seconds + dtr.minute * 60;
+  seconds := seconds + dtr.second;
+  ratio := seconds / 86400;
+  toNeralie := round(ratio * 1000000);
+ end;
+
 begin
 
  ShowDrawing;
+
  setRect(view, x, y, x + width, y + height);
  setDrawingRect(view);
 
  setRect(display, 0, 0, view.right - view.left - 15, view.bottom - view.top - 15);
  setRect(bounds, pad, pad, display.right - pad, display.bottom - pad);
- paintRect(display);
 
- penPat(white);
+ while not button do
+  begin
 
- frameRect(bounds);
- draw(bounds, 0.8, 0.3, 0.2, 0.4, 0.9, 0.2);
+   GetTime(dtr);
+   if dtr.second <> last then
+    begin
+     penPat(black);
+     paintRect(display);
+     penPat(white);
+     frameRect(bounds);
+     res := toNeralie(dtr);
+     writeln(res);
+     draw(bounds, res / 1000000, (res mod 100000) / 100000, (res mod 10000) / 10000, (res mod 1000) / 1000, (res mod 100) / 100, (res mod 10) / 10);
+    end;
+   last := dtr.second;
+  end;
 
 end.
